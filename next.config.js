109534effb8d27ee381b58d383d4c068a58f3985
soyeon <csea9000@naver.com>
2023-12-22 16:22:04 +0900

    Fix: 배포시 aggressiveFrontEndNavCaching 에러 수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  runtimeCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
   disable: process.env.NODE_ENV === 'development',
