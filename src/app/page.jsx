34b350c8e4812491b0658e182ffdbb6f76debf4c
soyeon <csea9000@naver.com>
2024-05-14 17:54:55 +0900

    Fix: 필요없는 코드 제거

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,9 +15,8 @@ import { isTemporaryState } from '@/store/initInfo/atom';
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
-  const [isClient, setIsClient] = useState(false);
   const [welcomeText, setWelcomeText] = useState('반가워요 저는 이오지오에요');
-  const [isFirst, setIsFirst] = useState(null);
+  const [isLogin, setIsLogin] = useState(null);
   const isTemp = useRecoilValue(isTemporaryState);
   const router = useRouter();
 
@@ -26,9 +25,9 @@ export default function Home() {
       window.localStorage.getItem('access_token') === null ||
       window.localStorage.getItem('access_token') === ''
     ) {
-      setIsFirst(true);
+      setIsLogin(false);
     } else {
-      setIsFirst(false);
+      setIsLogin(true);
     }
   }, []);
 
@@ -41,15 +40,12 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    if (isFirst && !isTemp) {
-      router.prefetch('/onboarding');
+    if (!isLogin && !isTemp) {
       router.push('/onboarding');
     }
-  }, [isFirst]);
+  }, [isLogin, isTemp, router]);
 
   useEffect(() => {
-    setIsClient(true);
-
     const toggleText = () => {
       setWelcomeText((prev) =>
         prev === '반가워요 저는 이오지오에요'
@@ -63,72 +59,70 @@ export default function Home() {
     return () => clearInterval(intervalEvent);
   }, []);
 
-  return isFirst && !isTemp ? (
+  console.log('isLogin', isLogin);
+  console.log('isTemp', isTemp);
+
+  return showSplash ? (
     <SplashScreen />
   ) : (
-    isClient &&
-      (showSplash ? (
-        <SplashScreen />
-      ) : (
-        <div className="h-full">
-          <div className="h-16 w-full py-2 px-5 flex items-center justify-between">
-            <Link href="/">
-              <Logo width="64" height="100%" className="cursor-pointer" />
-            </Link>
-            <Link href="/mypage">
-              <MypageIcon className="cursor-pointer " />
-            </Link>
-          </div>
-          <div className="flex flex-col justify-between h-[calc(100%-64px)] pt-8 sm:pt-10 overflow-y-auto">
+    <div className="h-full">
+      <div className="h-16 w-full py-2 px-5 flex items-center justify-between">
+        <Link href="/">
+          <Logo width="64" height="100%" className="cursor-pointer" />
+        </Link>
+        <Link href="/mypage">
+          <MypageIcon className="cursor-pointer " />
+        </Link>
+      </div>
+      <div className="flex flex-col justify-between h-[calc(100%-64px)] pt-8 sm:pt-10 overflow-y-auto">
+        <div>
+          <div className="flex justify-center text-center text-g200 text-xl mb-6 sm:mb-10">
             <div>
-              <div className="flex justify-center text-center text-g200 text-xl mb-6 sm:mb-10">
-                <div>
-                  AI 이오지오가 추천하는 <br />
-                  나만의 추천 영화 보러가는 거,
-                  <span className="font-semibold">오때?</span>
-                </div>
-              </div>
-              <div className="px-8 flex justify-center mb-4 sm:mb-8">
-                <div className="shadow-square text-center py-4 px-10 rounded-xl text-sm text-g100 sm:w-[300px]">
-                  {welcomeText}
-                </div>
-              </div>
-
-              <div
-                className="border-w border-main bg-cover bg-center bg-no-repeat w-full h-64 sm:bg-contain flex justify-center"
-                style={{
-                  backgroundImage: 'url("/imgs/bg-home.png")',
-                }}
-              >
-                <Image
-                  src="/imgs/home-character.png"
-                  alt="character img"
-                  width={270}
-                  height={270}
-                  style={{ height: 'fit-content' }}
-                />
-              </div>
+              AI 이오지오가 추천하는 <br />
+              나만의 추천 영화 보러가는 거,
+              <span className="font-semibold">오때?</span>
             </div>
-
-            <div className=" px-6 py-6 flex flex-col">
-              <Button
-                isLink={true}
-                href="/chat"
-                textColor="text-white"
-                bgColor="bg-main"
-                text="이오지오와 대화 시작하기"
-              />
-              <Button
-                isLink={true}
-                href="/mypage"
-                textColor="text-white"
-                bgColor="bg-v100"
-                text="나의 콘텐츠 노트 보러가기"
-                styleClass="mt-3"
-              />
+          </div>
+          <div className="px-8 flex justify-center mb-4 sm:mb-8">
+            <div className="shadow-square text-center py-4 px-10 rounded-xl text-sm text-g100 sm:w-[300px]">
+              {welcomeText}
             </div>
+          </div>
+
+          <div
+            className="border-w border-main bg-cover bg-center bg-no-repeat w-full h-64 sm:bg-contain flex justify-center"
+            style={{
+              backgroundImage: 'url("/imgs/bg-home.png")',
+            }}
+          >
+            <Image
+              src="/imgs/home-character.png"
+              alt="character img"
+              width={270}
+              height={270}
+              style={{ height: 'fit-content' }}
+            />
           </div>
         </div>
-      ))
+
+        <div className=" px-6 py-6 flex flex-col">
+          <Button
+            isLink={true}
+            href="/chat"
+            textColor="text-white"
+            bgColor="bg-main"
+            text="이오지오와 대화 시작하기"
+          />
+          <Button
+            isLink={true}
+            href="/mypage"
+            textColor="text-white"
+            bgColor="bg-v100"
+            text="나의 콘텐츠 노트 보러가기"
+            styleClass="mt-3"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
